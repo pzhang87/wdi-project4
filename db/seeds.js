494e2dac817b0ce39d323bb17532cad542349ae4
Peter Zhang <pzhang87@gmail.com>
2016-01-15 11:25:56 -0500

    fix seeds

--- db/seeds.js
+++ db/seeds.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-// var conn = mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/pr-vods')
+var conn = mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/pr-vods')
 var PlayerModel = require("../models/player")
 var VideoModel = require("../models/video")
 
