8a90943dc986f26784a098fd03e625c31f675b07
Peter Zhang <pzhang87@gmail.com>
2016-01-15 11:05:15 -0500

    fix url in model/collection

--- public/js/collections/players.js
+++ public/js/collections/players.js
@@ -1,4 +1,5 @@
 App.Collections.Players = Backbone.Collection.extend({
   model: App.Models.Player,
-  url: "http://localhost:3000/players",
+  // url: "http://localhost:3000/players",
+  url: "http://mdva-ssbm-pr.herokuapp.com/players"
 })
--- public/js/models/player.js
+++ public/js/models/player.js
@@ -1,5 +1,5 @@
 App.Models.Player = Backbone.Model.extend({
-  urlRoot: 'http://localhost:3000/players',
+  urlRoot: "http://mdva-ssbm-pr.herokuapp.com/players",
 
   initialize: function(){
     console.log("New Player model created")
